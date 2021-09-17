e0ae02df6fa4ab84649ec8f5cc86303169489203
Ramunechoco <48040337+ramunechoco@users.noreply.github.com>
2021-09-17 19:15:14 +0700

    add pooling layer

--- LEMBUT/layers.py
+++ LEMBUT/layers.py
@@ -39,3 +39,24 @@ class Detector(Layer):
         output = relu(X)
         return output
 
+class Pooling(Layer):
+    def __init__(self, units: int, activation: str, name: str, size=2, stride=2) -> None:
+        super().__init__(activation, name)
+        self.units = units
+        self.size = size
+        self.stride = stride
+
+    def __call__(self, X: np.ndarray) -> np.ndarray:
+        return self.pool(X)
+
+    def pool(self, X: np.ndarray) -> np.ndarray:
+        output = np.zeros((np.uint16((X.shape[0]-self.size+1)/self.stride+1), np.uint16((X.shape[1]-self.size+1)/self.stride+1), X.shape[-1]))
+        for map_num in range(X.shape[-1]):
+            r2 = 0
+            for r in np.arange(0,X.shape[0]-self.size+1, self.stride):
+                c2 = 0
+                for c in np.arange(0, X.shape[1]-self.size+1, self.stride):
+                    output[r2, c2, map_num] = np.max([X[r:r+self.size,  c:c+self.size]])
+                    c2 = c2 + 1
+                r2 = r2 +1
+        return output

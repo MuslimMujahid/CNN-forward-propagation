e74044943845a53cdc68ccaf679e9e40a25a2529
Muslim Al-Mujahid <muslim@xcidic.com>
2021-09-17 20:58:36 +0700

    convolution function

--- LEMBUT/util.py
+++ LEMBUT/util.py
@@ -1,17 +1,25 @@
 import numpy as np
 from PIL import Image
 
-def imgToMat(filepath: str):
-  file = Image.open(filepath, "r")
-  img = np.array(file)
-  b, g, r = img[:,:,0], img[:,:,1], img[:,:,2]
-  return b, g, r
 
-# print("img")
-# print(img)
-# print("\nred channel\n")
-# print(r)
-# print("\ngreen channel\n")
-# print(g)
-# print("\nblue channel\n")
-# print(b )
+def imgToMat(filepath: str):
+    f = Image.open(filepath, "r")
+    img = np.array(f)
+    return img
+
+
+def conv(X: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+    x_height, x_width, x_channel = X.shape
+    k_height, k_width, k_channel = kernel.shape
+
+    f_height, f_width = (x_width - k_width + 1, x_height - k_height + 1)
+    feature_map = np.zeros(shape=(f_height, f_width))
+    for i in range(f_height):
+        for j in range(f_width):
+            result = 0
+            for k in range(x_channel):
+                result += np.sum(np.multiply(X[i:i+k_width,
+                                               j:j+k_width, k], kernel[:, :, k]))
+            feature_map[i, j] = result
+
+    return feature_map
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from LEMBUT.model import Sequential
 from LEMBUT import layers
 
 from PIL import Image
+from LEMBUT import util
 
 # model = Sequential()
 # model.add(layers.Dense(2, "sigmoid", "input"))
@@ -12,3 +13,11 @@ from PIL import Image
 # X = np.random.rand(3, 2)
 # y = model(X)
 # print(y)
+
+img = util.imgToMat("C:\\Users\\Muslim\\Pictures\\suit4.jpg")
+kernel = np.array([
+    [[1, 7, 1], [1, 3, 1], [3, 4, 1]],
+    [[3, 0, 11], [1, 1, 1], [1, 3, 7]],
+    [[1, 4, 4], [9, 1, 1], [1, 0, 0]]
+])
+print(util.conv(img, kernel))

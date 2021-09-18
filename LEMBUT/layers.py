301fc1eedc5381969de32488a47e66b692b5f6d8
Muslim Al-Mujahid <muslim@xcidic.com>
2021-09-18 23:28:01 +0700

    add model summary

--- LEMBUT/layers.py
+++ LEMBUT/layers.py
@@ -15,16 +15,24 @@ class Dense(Layer):
         self.units = units
         self.W = None
 
+    def __name__(self):
+        return "Dense"
+
     def __call__(self, X: np.ndarray) -> np.ndarray:
         return self.forward(X)
 
     def forward(self, X: np.ndarray) -> np.ndarray:
+        input_size = X.shape[0 if len(X.shape) == 1 else 1]
         if (not self.W):
-            self.W = np.random.rand(
-                X.shape[0 if len(X.shape) == 1 else 1], self.units)
+            self.W = np.random.rand(input_size, self.units)
 
         net = np.dot(X, self.W)
         output = self.activation(net)
+
+        # For summary
+        self.output_shape = f'(None, {output.shape[0]})'
+        self.param = (input_size+1)*self.units
+
         return output
 
 
@@ -46,6 +54,9 @@ class Conv2D(Conv):
     def __init__(self, filters: int, name: str = "conv2d", kernel_size: tuple = (3, 3), activation: str = "relu", padding: int = 0, stride: tuple = (1, 1), bias: int = 0):
         super().__init__(filters, name, kernel_size, activation, padding, stride, bias)
 
+    def __name__(self):
+        return "Conv2D"
+
     def __call__(self, X: np.ndarray) -> np.ndarray:
         return self.forward(X)
 
@@ -65,19 +76,30 @@ class Conv2D(Conv):
 
         # Detector
         feature_maps = ACTIVATION_FUNCTIONS["relu"](feature_maps)
+
+        # For summary
+        self.output_shape = f'(None, {feature_maps.shape[1]}, {feature_maps.shape[2]}, {feature_maps.shape[0]})'
+        self.param = self.filters * \
+            (self.kernel_size[0]*self.kernel_size[1]*x_channel+1)
+
         return feature_maps
 
 
 class Pooling(Layer):
-    def __init__(self, name: str = "max_pooling", size=2, stride=2) -> None:
-        super().__init__(name)
+    def __init__(self, name=None, size=2, stride=2, mode="max") -> None:
+        super().__init__(name if name is not None else (mode + "_pooling"))
         self.size = size
         self.stride = stride
+        self.mode = mode
+
+    def __name__(self):
+        return "Pooling"
 
     def __call__(self, X: np.ndarray) -> np.ndarray:
         return self.pool(X)
 
     def pool(self, X: np.ndarray) -> np.ndarray:
+        pool_function = np.mean if self.mode == "avg" else np.max
         x_channel, x_height, x_width = X.shape
         output = np.zeros([x_channel, (x_height-self.size) //
                           self.stride+1, (x_width-self.size)//self.stride+1])
@@ -85,8 +107,13 @@ class Pooling(Layer):
         for i in range(x_channel):
             for j in range(o_height):
                 for k in range(o_width):
-                    output[i, j, k] = np.max(
+                    output[i, j, k] = pool_function(
                         X[i, j*self.stride:j*self.stride+self.size, k*self.stride:k*self.stride+self.size])
+
+        # For summary
+        self.output_shape = f'(None, {output.shape[1]}, {output.shape[2]}, {output.shape[0]})'
+        self.param = 0
+
         return output
 
 
@@ -94,8 +121,17 @@ class Flatten(Layer):
     def __init__(self, name: str = "flatten") -> None:
         super().__init__(name)
 
+    def __name__(self):
+        return "Flatten"
+
     def __call__(self, X: np.ndarray) -> np.ndarray:
         return self.forward(X)
 
     def forward(self, X: np.ndarray) -> np.ndarray:
-        return X.flatten('F')
+        flatten = X.flatten('F')
+
+        # For summary
+        self.output_shape = f'(None, {flatten.shape[0]})'
+        self.param = 0
+
+        return flatten
--- LEMBUT/model.py
+++ LEMBUT/model.py
@@ -1,10 +1,12 @@
 import numpy as np
 from .layers import Layer
+from tabulate import tabulate
 
 
 class Sequential:
     def __init__(self, layers=[]):
         self.layers = layers
+        self.has_run = False
 
     def __call__(self, X: np.ndarray) -> np.ndarray:
         return self.predict(X)
@@ -13,8 +15,23 @@ class Sequential:
         self.layers.append(layer)
 
     def predict(self, X: np.ndarray) -> np.ndarray:
+        self.has_run = True
+
         output = X
         for layer in self.layers:
             output = layer(output)
 
         return output
+
+    def summary(self):
+
+        if not self.has_run:
+            print("Do predict first before get summary!")
+            return
+
+        table = []
+        heads = ["Layer (type)", "Output Shape", "Params"]
+        for layer in self.layers:
+            table.append([f'{layer.name} ({layer.__name__()})',
+                          layer.output_shape, layer.param])
+        print(tabulate(table, headers=heads, tablefmt="github"))

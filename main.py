f9b18dd65bd8cf8ac4d7919368d6261f63fff072
Muslim Al-Mujahid <muslim@xcidic.com>
2021-09-20 10:13:45 +0700

    tidying up

--- main.py
+++ main.py
@@ -9,27 +9,25 @@ from keras.datasets import mnist
 
 (trainX, trainY), (testX, testY) = mnist.load_data()
 
-
-
-# img = util.imgToMat("image.jpg", size=(32, 32))
-# print(img)
 model = Sequential()
-model.add(layers.Conv2D(filters=6, kernel_size=(5, 5), input_shape=(28, 28, 1)))
-model.add(layers.Pooling())
-model.add(layers.Conv2D(filters=16, kernel_size=(5, 5)))
-model.add(layers.Pooling())
+
+# Convolutional layer
+model.add(layers.Conv2D(name="conv_1", filters=6, kernel_size=(5, 5), input_shape=(28, 28, 1)))
+model.add(layers.Pooling(name="pooling_1"))
+model.add(layers.Conv2D(name="conv_2", filters=16, kernel_size=(5, 5)))
+model.add(layers.Pooling(name="pooling_2"))
 model.add(layers.Flatten())
-model.add(layers.Dense(units=120, activation="relu"))
-model.add(layers.Dense(units=84, activation="relu"))
-model.add(layers.Dense(units=10, activation="softmax"))
-# result = model(img)
+
+# Fully connected layer
+model.add(layers.Dense(name="dense_1", units=120, activation="relu"))
+model.add(layers.Dense(name="dense_2", units=84, activation="relu"))
+model.add(layers.Dense(name="dense_3", units=10, activation="softmax"))
+
+# Predict
 for i in range(10):
     img = np.array([trainX[i]])
-    # print(img.shape)
-    # print(trainX.shape)
-    # print(np.ndarray(trainX[i]).shape)
     result = model(img)
     print(result)
-    # print("result shape", result.shape)
 
+# Summary
 model.summary()
